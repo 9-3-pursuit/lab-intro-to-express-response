7a7e1a9c3958048922dc07bd858518433f6a76ce
Kerridene Small <Kerridenesmall@gmail.com>
2023-04-04 21:29:26 -0400

    Created the random order

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ const express = require("express");
 const app = express()
 const PORT = 3003
 
+const magic8Responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - Definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes, and signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+
+]
+
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
-// app.get("/about", (req, res) => {
-//     res.status(201)
-//     res.json({ message: "this is my first express app!" });
-// });
+
 app.get('/tim-gunn', (req, res) => {
     res.send('Make it work')
 });
@@ -45,6 +66,12 @@ app.get('/Hannibal-Smith', (req, res) => {
 
 app.get('/Harry-Callahan', (req, res) => {
     res.send("Go ahead, make my day")
+});
+
+app.get('/magic8', (req, res) => {
+    const getRandomQuote = Math.floor(Math.random() * magic8Responses.length)
+    const randomResponse = magic8Responses[getRandomQuote];
+    res.send(randomResponse)
 });
 
 app.listen(PORT, () => {
