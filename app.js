bffdcd324b45d822c07a8d418738e1b574ee1810
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-05 03:32:50 -0400

    add send to home

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", () => {
+app.get("/", (req, response) => {
   console.log("Home - Route");
+//   response.status()
+  response.send("Home - Route")
 });
 
 app.listen(3003, () => {
