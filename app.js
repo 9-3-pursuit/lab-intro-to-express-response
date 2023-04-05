ab3af473e513cc1e7d3f93c61a5283636a010646
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-05 03:48:30 -0400

    add contact

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+// Dependencies
 const express = require("express");
+
+// Configuration
 const app = express();
+const PORT = 3003;
 
 app.get("/", (req, response) => {
   console.log("Home - Route");
-//   response.status()
-  response.send("Home - Route")
+  response.status();
+  response.send("Home - Route");
 });
 
-app.listen(3003, () => {
-  console.log("Server is running on port 3003");
+app.get("/about", (req, res) => {
+  res.status(201);
+  res.json({ message: "first about app" });
+});
+
+app.get("/contact", (req, res) => {
+  res.status(201).json({ message: "first contact app" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 console.log("testing...");
