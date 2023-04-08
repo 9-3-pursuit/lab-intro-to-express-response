968148a5055f09c7463c8db805848072a035f53e
HaroldF415 <harold.f415@gmail.com>
2023-04-08 19:43:06 -0400

    seperating concerns

---
+++ data/speakerIMG.js
@@ -0,0 +1,6 @@
+const speakerIMG = {
+  emeril: "http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQ0zowDIrQKcGhjlOnimd5jcbV8cyx8i26dczJWPr53297lWPOFfHSI5xHGu1qi-tUYC7X0lZg1dB-9CNY",
+  stevemcgarrett: "https://64.media.tumblr.com/34ebd1cab2a50c9b643d6db623c6208e/9f90cc507d008b6d-0f/s1280x1920/2c63425d02aa1aada7b56744ff16bf288d52143d.jpg",
+};
+
+module.exports = speakerIMG;
