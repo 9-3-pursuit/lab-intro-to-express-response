c8176a9dbbd38ffc2aee3a08af4fdf0befcbd135
HaroldF415 <harold.f415@gmail.com>
2023-04-08 20:27:12 -0400

    updated paths to test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,14 +1,19 @@
 const app = require("../app");
 const request = require("supertest");
 
-const paths2test = require("/data/paths2test");
+const paths2test = require("../data/paths2test");
 
-describe("Test root path for this lab", () => {
-  test("It should respond to the GET method", () => {
-    return request(app)
-      .get("/")
-      .then((response) => {
-        expect(response.statusCode).toBe(200);
-      });
-  });
+describe("Testing status codes on different paths", () => {
+  for (let i = 0; i < paths2test.length; i++) {
+    const description = paths2test[i].description;
+    const path = paths2test[i].path;
+
+    test(`It should respond to the GET method for ${description}`, () => {
+      return request(app)
+        .get(path)
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+        });
+    });
+  }
 });
